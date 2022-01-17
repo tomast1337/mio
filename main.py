881f2b3cc44c18c6b37c941b601e51d1b46c377e
Gabriel de Brito <gabrielgbrito@icloud.com>
2022-01-17 17:16:52 -0300

    Added license and source functions

--- main.py
+++ main.py
@@ -17,10 +17,20 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    # help, license and source functions {{{
     if message.content.startswith("$help"):
         await message.channel.send("Hello, I'm Mio Akiyama, I'll help you download and listen to music.")
         await message.channel.send(help_content)
 
+    elif message.content.startswith("$license"):
+        await message.channel.send(content="GNU General Public License, third version.", file=File("LICENSE"))
+
+    elif message.content.startswith("$source"):
+        await message.channel.send("If you have git installed, you can do 'git clone https://github.com/gioiacyberpunk/mio' to download the source code.\nIf not, go to that url, click on 'Code' green button and 'Download ZIP'. Unzip the downloaded file to any location.")
+
+    # }}}
+
 
 client.run(auth_file.read())
+help_file.close()
 
