c8b349c8dd2bb5fee4f2d69ab39881bc02dcb22c
Gabriel de Brito <gabrielgbrito@icloud.com>
2022-02-01 19:29:03 -0300

    Ok so Mio is currently working

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from discord import *
+from pytube import *
+import os
+import shutil
+import sys
 
 client = Client()
 
@@ -17,7 +21,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    # help, license and source functions {{{
+    # HELP, LICENSE AND SOURCE {{{
     if message.content.startswith("$help"):
         await message.channel.send("Hello, I'm Mio Akiyama, I'll help you download and listen to music.")
         await message.channel.send(help_content)
@@ -27,6 +31,30 @@ async def on_message(message):
 
     elif message.content.startswith("$source"):
         await message.channel.send("If you have git installed, you can do 'git clone https://github.com/gioiacyberpunk/mio' to download the source code.\nIf not, go to that url, click on 'Code' green button and 'Download ZIP'. Unzip the downloaded file to any location.")
+
+    # }}}
+
+    # MAIN BOT FUNCTIONS {{{
+
+    elif message.content.startswith("$video"):
+
+        await message.channel.send("Downloading videos, please wait...")
+
+        channel_id = str(message.channel.id)
+        try:
+            os.mkdir(channel_id)
+        except FileExistsError:
+            print()
+
+        spl_message = message.content.split()
+        links = spl_message[1:]
+
+        for link in links:
+            
+            # this is ugly but it's the only way to do it
+            await message.channel.send(file=File(YouTube(link).streams.filter(only_audio=True).first().download(channel_id)))
+
+        shutil.rmtree(channel_id)
 
     # }}}
 
